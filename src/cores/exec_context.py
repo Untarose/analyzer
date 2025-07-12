b65165073cba1590cbcb71746d0e4f01aa2d3933
Untarose <yuukierubo1@gmail.com>
2025-07-12 13:31:37 +0900

    ExecContextクラスの修正

--- src/cores/exec_context.py
+++ src/cores/exec_context.py
@@ -10,9 +10,18 @@ class ExecContext:
     kwargs: Mapping[str, object] = field(default_factory=dict)
 
     def __post_init__(self):
+        """コンストラクタ作成時に呼び出される
+        """
         self._validation_func_args()
 
     def _validation_func_args(self):
+        """コンストラクトされたときに呼び出される。
+           関数funcの引数名と、指定したunits_selection, kwargsに指定したkey名が一致するかどうか
+           これは、ユーザにGroup、Unitを操作する際の関数と指定したGroup、Unitの明示を強要する。
+
+        Raises:
+            ValueError: 関数funcに**kwargsを使用することはできない
+        """
         sig = inspect.signature(self.func)
         print("=== PARAM KINDS ===")
         for name, param in sig.parameters.items():
@@ -20,8 +29,9 @@ class ExecContext:
         # **kwargs を禁止！
         if any(param.kind == inspect.Parameter.VAR_KEYWORD for param in sig.parameters.values()):
             raise ValueError("関数に **kwargs を含めることはできません。明示的に全ての引数を定義してください。")
-
+        # funcの引数リスト
         func_arg = list(sig.parameters.keys())
+        # self.のunits.selectionやkwargsのリスト
         unit_keys = list(self.units_selection.keys()) + list(self.kwargs.keys())
 
         missing = [arg for arg in func_arg if arg not in unit_keys]
