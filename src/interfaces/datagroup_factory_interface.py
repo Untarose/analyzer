96ec7d9e53b6cae1076df66ab45ee30651bf7476
Untarose <yuukierubo1@gmail.com>
2025-07-12 16:11:46 +0900

    DataFactoyInterfaceに_create_methodの追加

--- src/interfaces/datagroup_factory_interface.py
+++ src/interfaces/datagroup_factory_interface.py
@@ -1,11 +1,18 @@
 from abc import ABC, abstractmethod
+from pathlib import Path
 from interfaces.datagroup_interface import DataGroupInterface
 from interfaces.dataunit_interface  import DataUnitInterface
-
+from interfaces.meta_interface import MetaInterface
 class DataGroupFactoryInterface(ABC):
     @abstractmethod
-    def create(self, units: list[DataUnitInterface], name, path) -> DataGroupInterface:
+    def create(self, units: list[DataUnitInterface], name: str, path: Path) -> DataGroupInterface:
         """
         DataUnitの配列を用いてDataGroupのインスタンスを返す
+        """
+        pass
+    @abstractmethod
+    def _create_meta(self, name: str, path: Path) -> MetaInterface:
+        """
+        Metaを構築するメソッド
         """
         pass
